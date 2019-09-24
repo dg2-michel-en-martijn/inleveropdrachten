dd76cdf3c6568566688fa8138261ddd2a1569ef5
Martijn <127121@student.drenthecollege.nl>
2019-09-24 15:12:33 +0200

    variabelen en functies gemaakt

---
+++ Tic tac toe/JS/app.js
@@ -0,0 +1,25 @@
+const ticTacToe = new TicTacToe();
+ticTacToe.start();
+
+function TicTacToe() {
+  const board = new Board();
+  const humanPlayer = new HumanPlayer();
+  const humanPlayer2 = new HumanPlayer2();
+  let turn = 0;
+
+  this.start = function() {
+
+  }
+}
+
+function Board() {
+
+}
+
+function HumanPlayer() {
+
+}
+
+function HumanPlayer2() {
+
+}
