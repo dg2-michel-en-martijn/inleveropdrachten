4235d47de791d9999aac6a0569694d515358f277
Martijn <127121@student.drenthecollege.nl>
2019-09-25 10:37:23 +0200

    Fixed html, css and js with some help

--- Tic tac toe/JS/app.js
+++ Tic tac toe/JS/app.js
@@ -1,59 +1,28 @@
-/* JS TIC TAC TOE */
-const ticTacToe = new TicTacToe();
-ticTacToe.start();
+//variables and players.
+var players=[];
+var markers=["X", "O"];
+var whoseTurn = 0;
+players[0] = "Player1";
+players[1] = "Player2";
 
-function TicTacToe() {
-  const board = new Board();
-  const humanPlayer = new HumanPlayer(board);
-  const humanPlayer2 = new HumanPlayer2(board);
-  let turn = 0;
 
-  this.start = function() {
-    const config = { childList: true };
-    const observer = new MutationObserver(() => takeTurn());
-    board.positions.forEach((el) => observer.observe(el, config))
-    takeTurn();
-  }
-
-  function takeTurn() {
-
-    if (turn % 2 == 0) {
-      humanPlayer.takeTurn();
-    } else {
-      humanPlayer2.takeTurn();
-    }
-    turn++;
-  }
+// play the game
+function play(clickedDiv)
+{
+  console.log(clickedDiv);
+  clickedDiv.onclick = "";
+  clickedDiv.innerHTML = "<span id='XenO'>"+ markers[whoseTurn] +"</span>";
+  toggle();
 }
 
-function Board() {
-  this.positions = Array.from(document.querySelectorAll('.col'));
-}
-/* PLAYER 1 */
-function HumanPlayer(board) {
-    this.takeTurn = function() {
-    board.positions
-      .forEach(el => el.addEventListener('click', handleTurnTaken));
- }
-
-  function handleTurnTaken(event) {
-    event.target.innerText = 'X';
-    board.positions
-      .forEach(el => el.removeEventListener('click', handleTurnTaken));
+function toggle()
+{
+  if (whoseTurn == 0)
+  {whoseTurn = 1;
   }
-}
-
-/* PLAYER 2 */
-function HumanPlayer2(board) {
-  this.takeTurn = function() {
-    const availablePositions =
-      board.positions.filter((p) => p.innerText === '');
-      board.positions
-        .forEach(el => el.addEventListener('click', handleTurnTaken2));
-}
-  function handleTurnTaken2(event) {
-    event.target.innerText = 'O';
-    board.positions
-      .forEach(el => el.removeEventListener('click', handleTurnTaken2));
+  else
+  {whoseTurn = 0;
   }
+
+  document.getElementById('game-message').innerText = players[whoseTurn] + "'s Turn";
 }
