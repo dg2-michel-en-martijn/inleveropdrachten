676779e7bf117ac842e2949a019ded3d3556b0c2
Martijn <127121@student.drenthecollege.nl>
2019-09-30 09:09:27 +0200

    "Toevoegen" knop working

--- Name list/JS/app.js
+++ Name list/JS/app.js
@@ -1,2 +1,9 @@
 var leftArray = [];
 var rightArray = [];
+$(document).ready(function(){
+  $("#toevoegen").click(function(){
+    if($("#naam").val() != ""){
+      leftArray.push($("#naam").val() + "<br/>");
+      document.getElementById('blok1').innerHTML = leftArray.join("");
+    }
+  });
