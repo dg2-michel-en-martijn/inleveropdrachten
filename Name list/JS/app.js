017156d32756ac7dd99a5559ee321b70e5613216
Martijn <127121@student.drenthecollege.nl>
2019-09-30 09:14:04 +0200

    left button

--- Name list/JS/app.js
+++ Name list/JS/app.js
@@ -7,11 +7,18 @@ $(document).ready(function(){
       document.getElementById('blok1').innerHTML = leftArray.join("");
     }
   });
-  $("#rechts").click(function(){
+  $("#right").click(function(){
     rightArray.push(leftArray.join(""));
     rightArray.join("");
     leftArray = [];
     document.getElementById('blok1').innerHTML = leftArray.join("");
     document.getElementById('blok2').innerHTML = rightArray;
+  });
+  $("#left").click(function(){
+    leftArray.push(rightArray.join(""));
+    leftArray.join("");
+    rightArray = [];
+    document.getElementById('blok1').innerHTML = leftArray;
+    document.getElementById('blok2').innerHTML = rightArray.join("");
   });
 });
