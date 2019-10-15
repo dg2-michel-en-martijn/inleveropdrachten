854952d0573b93401ac946c7ee9e566bf2895126
Martijn <127121@student.drenthecollege.nl>
2019-10-15 09:19:02 +0200

    Woonplaats, adres, postcode en geboortedatum worden nu gecheckt

--- Invul formulier/JS/app.js
+++ Invul formulier/JS/app.js
@@ -14,7 +14,7 @@ function getAge(dateString) {
 $(document).ready(function(){
   var naam = $("#naam");
   var adres = $("#adres");
-  var plaats = $("#plaats");
+  var woonplaats = $("#woonplaats");
   var postcode = $("#postcode");
   var geboortedatum = $("#geboortedatum");
   var nationaliteit = $("#nationaliteit");
@@ -27,6 +27,41 @@ $(document).ready(function(){
     } else {
       $('#naam-error').fadeOut(300);
     }
+ })
 
-  })
+//adress
+ adres.keyup(() => {
+   if(!/^[a-zA-Z$]*\s[1-9]*$/.test(adres.val())){
+     $('#adress-error').fadeIn(300);
+   } else {
+     $('#adress-error').fadeOut(300);
+   }
+ })
+
+ //woonplaats
+ woonplaats.keyup(() => {
+   if(!/^[a-zA-z]*$/.test(woonplaats.val())){
+     $('#woonplaats-error').fadeIn(300);
+   } else {
+     $('#woonplaats-error').fadeOut(300);
+   }
+ })
+
+ //postcode
+ postcode.keyup(() => {
+   if(!/^[0-9]{4}\s[a-zA-Z]{2}$/.test(postcode.val())){
+     $('#postcode-error').fadeIn(300);
+   } else {
+     $('#postcode-error').fadeOut(300);
+   }
+ })
+
+ //geboortedatun
+ geboortedatum.keyup(() => {
+   if(/^\d{4}-\d{2}-\d{2}$/.test(geboortedatum.val())){
+     $('#geboortedatum-error').fadeIn(300);
+   } else {
+     $('#geboortedatum-error').fadeOut(300);
+   }
+ })
 });
