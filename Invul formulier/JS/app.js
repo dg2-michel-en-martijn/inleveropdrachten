3b4f7a99f3d9c729b081452755f68089d6e05f10
Martijn <127121@student.drenthecollege.nl>
2019-10-18 09:09:12 +0200

    cleaned up

--- Invul formulier/JS/app.js
+++ Invul formulier/JS/app.js
@@ -1,18 +1,7 @@
 var veldenNed = ["Naam:","Adres:","Woonplaats:", "Postcode:", "Geboortedatum:", "Nationaliteit:", "Beroep:", "Opmerkingen: "];
 var veldenEng = ["Name:","Address:","Residence:","Postalcode:","Date of Birth:","Nationality:","Work:", "Comments: "];
 
-//Formule leeftijdberekening
-function getAge(dateString) {
-    var today = new Date();
-    var birthDate = new Date(dateString);
-    var age = today.getFullYear() - birthDate.getFullYear();
-    var m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-    }
-    return age;
-}
-
+//leeftijdberekening +  datepicker
 $(document).ready(function(){
   var age = "";
   $('#dob').datepicker({
